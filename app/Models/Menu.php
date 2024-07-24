9a540b4c895f37eefe7467c003acbf1cf458d7e0
Rashika <60001683+rashika10@users.noreply.github.com>
2024-07-24 16:35:36 +0545

    not change

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -13,7 +13,7 @@ class Menu extends Model
     protected $fillable = ['menu_id', 'menu_name','status'];
 
     public function permissions(){
-        // return $this->belongsToMany(Permission::class, 'menu_permissions','menu_id', 'button_id');
+        // 
         return $this->belongsToMany(Permission::class, 'role_menu_permissions', 'menu_id', 'permission_id')
         ->withPivot('role_id')
         ;
